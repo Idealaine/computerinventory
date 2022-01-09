8ceb221e6903730de9ba7de66969c73d754995e3
mirlaine <janmirlaine.idea@yahoo.com1>
2022-01-09 12:56:55 +0800

    first commit

---
+++ admin/index.php
@@ -0,0 +1,23 @@
+<?php include('session.php'); ?>
+<?php include('header.php'); ?>
+<body>
+<div id="wrapper">
+<?php include('navbar.php'); ?>
+<div style="height:50px;"></div>
+<div id="page-wrapper">
+<div class="container-fluid">
+<div style="height:40px;"></div>
+	<div class="container" class="navbar-brand" > 
+		<h1> INVENTORY SYSTEM </h1>
+		<h5> Hardware inventory management is the recording and tracking of hardware assets and inventory in an organization. </h5>
+		
+	</div>	
+
+	<img src="../upload/cover1.jpg" class="thumbnail" style="height:550px; width:1170px;">
+</div>
+</div>
+</div>
+<?php include('script.php'); ?>
+<?php include('modal.php'); ?>
+</body>
+</html>
